01b991b0a1216c0fb2f8d323e1656c3f2ae1b59c
ambarino <emaildeteo@gmail.com>
2024-05-14 18:19:09 +0200

    add test inventory

---
+++ inventory/inventory_ui.gd
@@ -0,0 +1,21 @@
+extends Control
+
+var isOpen: bool = false
+
+func _ready():
+	close()
+	
+func _process(delta):
+	if Input.is_action_just_pressed("inventory"):
+		if(isOpen):
+			close()
+		else:
+			open()
+	
+func open():
+	visible = true
+	isOpen = true
+
+func close():
+	visible = false
+	isOpen = false
