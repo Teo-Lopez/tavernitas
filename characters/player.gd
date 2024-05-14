f558dc8fef77df8c03c7efa9efed9c4bfa10df08
DESKTOP-QBVEM9S\Teo <emaildeteo@gmail.com>
2024-05-14 17:49:19 +0200

    first commit

---
+++ characters/player.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var direction = Input.get_vector('ui_left', 'ui_right', 'ui_up', 'ui_down')
+	
+	velocity = direction*200
+	move_and_slide()
